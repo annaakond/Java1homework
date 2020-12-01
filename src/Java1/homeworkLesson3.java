b358954b2371cff323a7011aed7ee9a7e5ee5ef0
geekbrains <annaakond@gmail.com>
2020-12-02 00:48:28 +0200

    homework Lesson 3. game "guess digit"

--- src/Java1/homeworkLesson3.java
+++ src/Java1/homeworkLesson3.java
@@ -1,5 +1,86 @@
 package Java1;
 
+import org.w3c.dom.ls.LSOutput;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class homeworkLesson3 {
-    
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
+
+
+    public static void main(String[] args) {
+        gameRiddle();
+
+    }
+
+       private static void gameRiddle() {
+
+        compareDigit();
+        repeatGame();
+    }
+
+
+    private static void setNumber() {
+
+
+        }
+
+
+    private static void enterDigit() {
+
+
+
+
+
+
+    }
+
+    public static void compareDigit() {
+        int digit = rand.nextInt(10);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Угадайте число");
+        int printedDigit = scanner.nextInt();
+        scanner.close();
+
+        if (printedDigit < digit); {
+        System.out.println("Не угадали. Ваше число меньше задуманного.");
+        }
+        if (printedDigit > digit); {
+        System.out.println("Не угадали. Ваше число больше задуманного.");
+        }
+        if (printedDigit == digit){
+            System.out.println("Отлично!");
+        }
+
+    }
+        private static void repeatGame() {
+                while (true) {
+            System.out.println("Повторить игру ещё раз? 1 - да / 0 - нет" );
+            chooseAction();
+               }
+        }
+
+            private static void chooseAction() {
+                switch(in.next) {
+                    case "1":
+                        enterDigit();
+                    case "0":
+                        doExit();
+                }
+
+            }
+            private static void doExit() {
+                System.exit(0);
+            }
+
+
+
+
+
 }
+
+
+
